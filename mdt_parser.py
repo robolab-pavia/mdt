31c659d67d8db8764989e2c182d5c70f4a5cb494
andre <andrea.catena01@universitadipavia.it>
2020-12-22 19:38:56 +0100

    updated mdt_parser with GPLv3

--- mdt_parser.py
+++ mdt_parser.py
@@ -1,4 +1,8 @@
 """
+    <Markdown file reader from terminal.>
+    Copyright (C) <2020>  <Catena Andrea, Facchinetti Tullio, Benetti Guido>
+"""
+"""
     Markdown in terminal is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -10,9 +14,8 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with Markdown in terminal.  If not, see <http://www.gnu.org/licenses/>.
 """
-
 import json
 import textwrap
 import webbrowser
@@ -28,6 +31,7 @@ from mdt_render import MDTRenderer
 from mistletoe import Document
 import os
 
+
 # Global key bindings.
 bindings = KeyBindings()
 
@@ -42,7 +46,7 @@ class AppState:
 
     #margin
     col = None
-    rmargin = None
+    rmargin = 0
 
     # reference text
     plain_text_ = ""
@@ -74,20 +78,19 @@ class AppState:
     urls = {}
     line_link_number = []
 
+
 def change_history_container():
     file_manager = []
     file_manager = map(lambda x: x[1], AppState.history)
     file_manager = list(file_manager)
     file_manager[AppState.history_index] = '[' + file_manager[AppState.history_index] + ']'
     line = ' '.join(file_manager)
-
-
     AppState.root_container.get_children()[1].content.buffer.text = AppState.history_template.format(line)
 
 
-#go back in file.md history
 @bindings.add('left')
 def go_back_history(event):
+    """Go back to the previous file in the history."""
     AppState.urls = {}
     if AppState.history_index > 0:
         AppState.history_index -= 1
@@ -99,6 +102,7 @@ def go_back_history(event):
         return
     except:
         pass
+
 
 @bindings.add('right')
 def go_forward_history(event):
@@ -114,39 +118,42 @@ def go_forward_history(event):
     except:
         pass
 
-# scroll down the file
 @bindings.add('down')
 def get_down(event):
+    """Scroll down the file one line."""
     if AppState.end_position < len(AppState.rendered.split("\n")):
         AppState.start_position += 1
 
-# go to the beginning of the file
+
 @bindings.add('home')
 def beginning_of_file(event):
+    """Go to the beginning of the file."""
     AppState.start_position = 0
 
-# go to the end of the file
+
 @bindings.add('end')
 def end_of_file(event):
+    """Go to the end of file."""
     AppState.start_position = len(AppState.rendered.split('\n')) - AppState.app.renderer.output.get_size()[0]
 
-# scroll up the file
 @bindings.add('up')
 def get_up(event):
+    """Scroll up the file one line."""
     if AppState.start_position > 0:
         AppState.start_position -= 1
 
-# page up the file
 @bindings.add('pageup')
 def page_up(event):
+    """Page up the file."""
     if AppState.start_position > AppState.app.renderer.output.get_size()[0]:
         AppState.start_position -= AppState.app.renderer.output.get_size()[0]
     else:
         AppState.start_position = 0
 
-# page down the file
+
 @bindings.add('pagedown')
 def page_down(event):
+    """One page down the file."""
     if AppState.start_position < len(AppState.rendered.split('\n'))-2*AppState.app.renderer.output.get_size()[0]:
         AppState.start_position += AppState.app.renderer.output.get_size()[0]
     else:
@@ -158,17 +165,17 @@ def page_down(event):
 def exit_(event):
     """
     Pressing Ctrl-Q will exit the user interface.
-
     Setting a return value means: quit the event loop that drives the user
     interface and return this value from the `Application.run()` call.
     """
     event.app.exit()
 
-# go to the next link
+
 @bindings.add('tab')
 def link_after(event):
+    """Go to the next link."""
     if AppState.current_link >= len(list(AppState.urls))-1:
-        AppState.current_link = 0
+        AppState.current_link = -1
     if len(AppState.urls) != 0:
         if AppState.current_link < len(list(AppState.urls))-1:
             AppState.current_link += 1
@@ -183,9 +190,9 @@ def link_after(event):
         AppState.p_text = AppState.p_text.replace('\007', '').replace('[' + titolo + '](' + link + ')', '[\007' + titolo + '](' + link + ')')
 
 
-# go to the previous link
 @bindings.add('s-tab')
 def link_before(event):
+    """Go to the previous link."""
     if len(AppState.urls) != 0:
         if AppState.current_link > 0:
             AppState.current_link -= 1
@@ -201,9 +208,9 @@ def link_before(event):
             AppState.p_text = AppState.p_text.replace('\007', '').replace('[' + titolo + '](' + link + ')',
                                                                                           '[\007' + titolo + '](' + link + ')')
 
-# open the choosen link
 @bindings.add('enter')
 def enter_link(event):
+    """Open the selected link."""
     if len(AppState.urls) > 0:
         link_ = AppState.urls[list(AppState.urls)[AppState.current_link]]
         link_name = list(AppState.urls)[AppState.current_link]
@@ -226,8 +233,8 @@ def enter_link(event):
             except:
                 pass
 
-# resize window every time (callable)
 def wrap_text(app):
+    """Resize window every time (callable)."""
     AppState.app = app
     fd = Document(AppState.p_text)
     with MDTRenderer(dix=AppState.custom_themes, global_ref=AppState.urls, app=app) as render:
@@ -318,46 +325,60 @@ def run(interactive):
         AppState.app.run()
 
 
+def check_theme_arg(ctx, param, value):
+    """Callback for checking the value of the theme ID."""
+    if value <= 0:
+        raise click.BadParameter('negative value {}'.format(value))
+    n_themes = len(os.listdir('themes'))
+    if value > n_themes:
+        raise click.BadParameter("required theme ID in [1..{}], given {}".format(n_themes, value))
+    return value
+
+
+def check_col_arg(ctx, param, value):
+    """Callback for checking the value of the columns."""
+    if value is None:
+        return value
+    if value <= 0:
+        raise click.BadParameter('non-positive value {}'.format(value))
+    return value
+
+
+def check_rmargin_arg(ctx, param, value):
+    """Callback for checking the value of the right margin."""
+    if value is None:
+        return value
+    if value < 0:
+        raise click.BadParameter('negative value {}'.format(value))
+    return value
+
+
 @click.command()
 @click.argument('mdfile', required=False)
 @click.option('--gallery', help='Print a demo gallery of the available themes.', is_flag=True)
 @click.option('-i', help='Interactive mode.', is_flag=True)
 @click.option('-l', help='List all the default themes.', is_flag=True)
-@click.option('--col', help='Set the text width in number of columns.', type=int)
-@click.option('--rmargin', help='Set the right right margin.', type=int, default=0)
-@click.option('--theme', default=1, help='Choose a default theme by ID.', type=int)
-@click.option('--theme_file', help='Choose a theme file.')
+@click.option('--col', callback=check_col_arg, help='Set the text width in number of columns.', type=int)
+@click.option('--rmargin', callback=check_rmargin_arg, help='Set the right right margin.', type=int, default=0)
+@click.option('--theme', default=1, callback=check_theme_arg, help='Choose a default theme by ID.', type=int)
+@click.option('--theme-file', help='Choose a theme file.')
 def mdt(mdfile, theme, gallery, i, l, col=None, rmargin=0, theme_file=None):
+    os.system("clear")
     """Main function."""
     if col != None and rmargin != 0:
         print("The options --col and --rmargin can not be used at the same time.")
         exit(1)
     if gallery == True:
         show_gallery()
-        exit(1)
+        return
     if l == True:
         show_theme_list()
-        exit(1)
-    theme_ = None
-    if col != None and col < 0:
-        print('Invalid number of columns: {}'.format(col))
-        exit(1)
+        return
 
-    if theme <= 0:
-        print('Invalid theme ID: {}'.format(theme))
-        exit(1)
-    n_themes = len(os.listdir('themes'))
-    if theme > n_themes:
-        print("Max theme ID: {}".format(n_themes))
-        exit(1)
-
-    if rmargin < 0:
-        print('Invalid rmargin: {}'.format(rmargin))
-        exit(1)
-
-    if theme_file == None:
+    theme_ = 'themes/' + sorted(os.listdir('themes'))[0]
+    if theme is not None:
         theme_ = 'themes/' + sorted(os.listdir('themes'))[theme-1]
-    else:
+    if theme_file is not None:
         theme_ = theme_file
 
     try:
@@ -381,7 +402,6 @@ def mdt(mdfile, theme, gallery, i, l, col=None, rmargin=0, theme_file=None):
     AppState.col = col
     AppState.rmargin = rmargin
     run(i)
-
 
 
 def main():
